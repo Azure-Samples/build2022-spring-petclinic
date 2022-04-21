edcc3f4139a0e6a6f7e592078aea2eb268380602
Rory Preddy <>
2022-04-21 12:06:09 +0200

    MSbuild prep

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -29,6 +29,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.devtools.restart.enabled", "false");
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
 
