c12a2da53948f19e83515788838bb65724eea7bf
Rory Preddy <rorypreddy@gmail.com>
2022-08-30 16:51:52 +0000

    commit

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -90,7 +90,7 @@ class OwnerController {
 	public String processFindForm(@RequestParam(defaultValue = "1") int page, Owner owner, BindingResult result,
 			Model model) {
 
-		System.out.println(owner.getLastName());
+		System.out.println("Owner:" + owner.getLastName());
 
 		// allow parameterless GET request for /owners to return all records
 		if (owner.getLastName() == null) {
